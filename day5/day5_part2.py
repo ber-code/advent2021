2ad91098d023c9d41c9cfea0bd23578c8f87e3f1
ber-code-go <bspamco@gmail.com>
2021-12-07 19:05:27 -0500

    day 5 solutions

---
+++ day5/day5_part2.py
@@ -0,0 +1,46 @@
+import re
+
+lines = []
+rows = cols = 0
+f = open("input_day5.txt", "r")
+for line in f:
+    new_line = list(map(int, re.split(",| -> ", line.replace("\n", ""))))
+    lines.append(new_line)
+    rows = max(rows, new_line[1] + 1, new_line[3] + 1)
+    cols = max(cols, new_line[0] + 1, new_line[2] + 1)
+f.close()
+vent_map = list()
+for row in range(rows):
+    vent_map.append([0] * cols)
+
+last_vents = list(vent_map)
+for line in lines:
+    x1, x2, y1, y2 = line[1], line[3], line[0], line[2]
+    if x1 == x2:
+        left_col, right_col = min(y1, y2), max(y1, y2)
+        for col_idx in range(left_col, right_col + 1):
+            vent_map[x1][col_idx] += 1
+    elif y1 == y2:
+        top_row, bot_row = min(x1, x2), max(x1, x2)
+        for row_idx in range(top_row, bot_row + 1):
+            vent_map[row_idx][y1] += 1
+    else:
+        if x1 > x2:
+            x1, x2, y1, y2 = x2, x1, y2, y1
+        if y1 < y2:
+            while y1 <= y2:
+                vent_map[x1][y1] += 1
+                x1 += 1
+                y1 += 1
+        elif y1 > y2:
+            while y1 >= y2:
+                vent_map[x1][y1] += 1
+                x1 += 1
+                y1 -= 1
+
+count = 0
+for row in vent_map:
+    for point in row:
+        if point >= 2:
+            count += 1
+print(count)
